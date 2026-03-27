f643c1fd44658ad20e848e5dc0dc5d8a58ef5d70
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-03-27 13:31:12 +0000

    Add all models

---
+++ models/Messages.cjs
@@ -0,0 +1,10 @@
+const messages = [
+    {
+        id: crypto.randomUUID(),
+        text: 'Welcome to Mini Message Board',
+        user: 'admin',
+        added: new Date(),
+    },
+];
+
+module.exports = messages;
