9b3b9bea5f928abd185e20029d5ddf038f87c5bd
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-03-27 13:30:18 +0000

    Add app.cjs and package.json

--- app.cjs
+++ app.cjs
@@ -1,18 +1,36 @@
 const express = require('express');
 const path = require('path');
+const IndexRouter  = require('./routes/IndexRouter.cjs');
+const { getAppData } = require('./controllers/AppDataController.cjs');
 
+
+// ==================== INITIALIZATION ====================
+const {TITLE, PORT} = getAppData();
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+//==================== SETUP ====================
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) =>{
-    res.render('index', {appName: 'Mini Message Board'});
+//==================== ROUTING ====================
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
+app.use('/', IndexRouter);
+
+app.use((req, res) => {
+    res.status(404).render('error', {title: TITLE, errMessage: 'Path does not exist'});
 });
 
-const PORT = process.env.PORT || 8080;
+//==================== ERROR HANDLING ====================
+app.use((err, req, res, next) => {
+    if(res.headersSent){
+        next(err);
+    }
+    console.error(err);
+    res.status(err.status || 500).render('error', {title: TITLE, errMessage: 'Something seems to be broken!'});
+});
+
+//==================== LISTEN TO REQUESTS ====================
 
 app.listen(PORT, err => {
     if(err) throw err;
